af2526489fb6305c5fec5d709723f46b423d2350
mpsingh17 <mpsingh17@outlook.co.nz>
2020-06-15 22:28:38 +1200

    Associate user with make.

--- VegaStore.Core/Entities/EntityBase.cs
+++ VegaStore.Core/Entities/EntityBase.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace VegaStore.Core.Entities
@@ -7,6 +8,8 @@ namespace VegaStore.Core.Entities
     public class EntityBase
     {
         public int Id { get; set; }
+        [Required]
+        public string UserId { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
     }
---
+++ VegaStore.UI/Migrations/20200615100343_AddUserIdColumnToEntityBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace VegaStore.UI.Migrations
+{
+    public partial class AddUserIdColumnToEntityBase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UserId",
+                table: "Makes",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Makes");
+        }
+    }
+}
