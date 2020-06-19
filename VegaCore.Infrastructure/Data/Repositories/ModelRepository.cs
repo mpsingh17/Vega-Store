db1b49371783d861c541c1a6071e49619711ecdc
mpsingh17 <mpsingh17@outlook.co.nz>
2020-06-19 21:01:32 +1200

    Repository for Model resource with basic get actions.

---
+++ VegaCore.Infrastructure/Data/Repositories/ModelRepository.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VegaCore.Infrastructure.Data;
+using VegaStore.Core.Entities;
+using VegaStore.Core.Repositories;
+
+namespace VegaStore.Infrastructure.Data.Repositories
+{
+    public class ModelRepository : Repository<Model>, IModelRepository
+    {
+        public ModelRepository(EFCoreContext context) : base(context) {}
+
+        public IEnumerable<Model> Models => GetAll(trackChanges: false).ToListAsync().Result;
+
+        public async Task<Model> GetSingleModelAsync(int id, bool trackChanges)
+        {
+            return await FindByCondition(m => m.Id.Equals(id), trackChanges)
+                .SingleOrDefaultAsync();
+        }
+    }
+}
---
+++ VegaStore.Core/Repositories/IModelRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using VegaStore.Core.Entities;
+
+namespace VegaStore.Core.Repositories
+{
+    public interface IModelRepository : IRepository<Model>
+    {
+        IEnumerable<Model> Models { get; }
+        Task<Model> GetSingleModelAsync(int id, bool trackChanges);
+    }
+}
