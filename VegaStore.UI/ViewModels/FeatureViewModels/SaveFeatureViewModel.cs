f2098d25921a18145cda2393b98df4da8a70238d
mpsingh17 <mpsingh17@outlook.co.nz>
2020-06-23 18:56:19 +1200

    put saveFeatureVM in correct folder.

--- VegaStore.UI/ViewModels/FeatureViewModels/SaveFeatureViewModel.cs
+++ VegaStore.UI/ViewModels/FeatureViewModels/SaveFeatureViewModel.cs
@@ -4,7 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace VegaStore.UI.ViewModels
+namespace VegaStore.UI.ViewModels.FeatureViewModels
 {
     public class SaveFeatureViewModel
     {
