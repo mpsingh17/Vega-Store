13d4e1ebbc1b96c62e8d210f311b20785b92fbfe
mpsingh17 <mpsingh17@outlook.co.nz>
2020-07-15 22:55:23 +1200

    experimenting cookies.

---
+++ VegaStore.UI/Controllers/CookiesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Options;
+
+namespace VegaStore.UI.Controllers
+{
+    public class CookiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            var cookies = Request.Cookies;
+
+            return View(cookies);
+        }
+
+        public IActionResult Create()
+        {
+            var key = "navbar_color";
+            var value = "blue";
+            int? expireTime = 2;
+
+            var cookieOptions = new CookieOptions();
+
+            if (expireTime.HasValue)
+                cookieOptions.Expires = DateTime.Now.AddMinutes(expireTime.Value);
+            else
+                cookieOptions.Expires = DateTime.Now.AddMilliseconds(10);
+
+            Response.Cookies.Append(key, value, cookieOptions);
+
+            return View();
+        }
+    }
+}
