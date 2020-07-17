480425350788445bf159bc0573bf8373c7f22708
mpsingh17 <mpsingh17@outlook.co.nz>
2020-07-17 21:21:10 +1200

    Display vehicles to public users.

---
+++ VegaStore.UI/Controllers/VehiclesController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using VegaStore.Core.Entities;
+using VegaStore.Core.Repositories;
+using VegaStore.Core.RequestFeatures;
+using VegaStore.UI.ViewModels.Public.VehicleViewModels;
+
+namespace VegaStore.UI.Controllers
+{
+    public class VehiclesController : Controller
+    {
+        private readonly IRepositoryManager _repository;
+        private readonly IMapper _mapper;
+
+        public VehiclesController(IRepositoryManager repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IActionResult> Index(VehicleParameters vehicleParameters)
+        {
+            var vehiclesInDb = await _repository.Vehicles.GetAllVehiclesAsync(vehicleParameters, trackChanges: false);
+
+            var result = _mapper.Map<IEnumerable<ListVehiclesViewModel>>(vehiclesInDb);
+
+            return View(result);
+        }
+    }
+}
---
+++ VegaStore.UI/ViewModels/Public/VehicleViewModels/ListVehiclesViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VegaStore.UI.ViewModels.Public.VehicleViewModels
+{
+    public class ListVehiclesViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        [Display(Name = "Registered")]
+        public string IsRegistered { get; set; }
+
+        public string Color { get; set; }
+
+        public string Condition { get; set; }
+
+        public string Price { get; set; }
+
+        public string Model { get; set; }
+
+        public string CreatedAt { get; set; }
+    }
+}
