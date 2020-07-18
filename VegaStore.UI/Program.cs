cd32544a9b7afbbed81639752231562bd4200f3c
mpsingh17 <mpsingh17@outlook.co.nz>
2020-07-18 20:32:48 +1200

    logging with Serilog.

--- VegaStore.UI/Program.cs
+++ VegaStore.UI/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 using VegaStore.UI.Extensions;
 
 namespace VegaStore.UI
@@ -14,11 +15,35 @@ namespace VegaStore.UI
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().MigrateDatabase().Run();
+            // Read configuration from appsettings.json
+            var config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            // Initialize logger
+            Log.Logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(config)
+                .CreateLogger();
+
+            try
+            {
+                Log.Information("Application starting.");
+                CreateHostBuilder(args).Build().MigrateDatabase().Run();
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Something went wrong during application startup.");
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
